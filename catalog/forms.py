5932d223366d165ba111cabe01d03514b7638d37
Kvashin Valeriy <val_kv@mail.ru>
2024-05-24 09:59:56 +0200

    edit models and forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,10 +3,6 @@ from .models import Product, Version, Category
 
 
 class ProductForm(forms.ModelForm):
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана",
-                                      required=True)
-    image = forms.ImageField(blank=True, null=True)
-
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'category', 'image']
--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
